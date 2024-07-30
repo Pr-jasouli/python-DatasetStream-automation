19afd8492b1f0452b70f23a40b2f06f0d0a790da
David LJ Botton <david.botton@mail.com>
2024-07-30 17:55:02 +0200

    ChannelSynthesizer: parsing of specific VOO codes and adapt columns accordingly

--- ChannelSynthesizer/src/utils.py
+++ ChannelSynthesizer/src/utils.py
@@ -1,5 +1,28 @@
 import re
 
+VOO_INFO_CODES = {
+    "w VS": "VOOsport World",
+    "VS": "VOOsport",
+    "Pa": "Bouquet Panorama",
+    "Ci": "Option Ciné Pass",
+    "Doc": "Be Bouquet Documentaires",
+    "Div": "Be Bouquet Divertissement",
+    "Co": "Be Cool",
+    "Enf": "Be Bouquet Enfant",
+    "Sp": "Be Bouquet Sport",
+    "Sel": "Be Bouquet Selection",
+    "Inf": "Option Infos",
+    "Sen": "Option Sensation",
+    "Ch": "Option Charme",
+    "FF": "Family Fun",
+    "DM": "Discover More",
+    "CX": "Classé X",
+    "MX": "Man-X",
+    "B": "Bruxelles",
+    "G": "Comm. German",
+    "W": "Wallonie",
+}
+
 def get_provider_and_year(filename):
     provider_names = ['voo', 'orange', 'telenet']
     provider = None
@@ -21,7 +44,7 @@ def read_section_names(file_path):
         section_names = [line.strip() for line in f.readlines()]
     return section_names
 
-def parse_tsv(tsv_path, section_names):
+def parse_tsv(tsv_path, section_names, provider):
     with open(tsv_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
@@ -34,9 +57,35 @@ def parse_tsv(tsv_path, section_names):
             current_section = stripped_line
         elif not stripped_line.isdigit() and not re.match(r'^\d{1,3}$', stripped_line):
             if current_section:
-                data.append([current_section, stripped_line])
+                if provider == "Voo":
+                    stripped_line, additional_columns = handle_voo_info_codes(stripped_line)
+                    data.append([current_section, stripped_line] + additional_columns)
+                else:
+                    data.append([current_section, stripped_line])
 
     return data
+
+def handle_voo_info_codes(line):
+    additional_columns = []
+    words = line.split()
+    filtered_words = []
+    i = 0
+
+    while i < len(words):
+        matched = False
+        for j in range(len(words), i, -1):
+            potential_key = ' '.join(words[i:j])
+            if potential_key in VOO_INFO_CODES:
+                additional_columns.append(VOO_INFO_CODES[potential_key])
+                i = j  # Skip the matched key
+                matched = True
+                break
+        if not matched:
+            filtered_words.append(words[i])
+            i += 1
+
+    filtered_line = ' '.join(filtered_words)
+    return filtered_line, additional_columns
 
 def adjust_region_columns(df):
     updated_index = []
