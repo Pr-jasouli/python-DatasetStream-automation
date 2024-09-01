c3f43083bc93a6f8fd96689290979f9595e9a2f2
David LJ Botton <david.botton@mail.com>
2024-09-01 13:48:20 +0200

    PDF: Updated Excel enabler

--- ChannelSynthesizer/src/enablers/excel.py
+++ ChannelSynthesizer/src/enablers/excel.py
@@ -1,18 +1,36 @@
 from pathlib import Path
-from ChannelSynthesizer.src.parsers.excel_generator import process_providers
+from ChannelSynthesizer.src.utils import get_provider_and_year, read_section_names, parse_tsv, find_file_pairs, \
+    create_consolidated_excel
+
 
 def generate_excel_report(output_directory):
     """
-    Génére le rapport Excel consolidé à partir des fichiers de sections et de texte.
+    genere un rapport Excel consolide a partir des fichiers de section et de texte.
 
-    :param output_directory: Répertoire contenant les fichiers de sortie
+    :param output_directory: repertoire contenant les fichiers de sortie
+    :return: Le chemin vers le fichier Excel genere
     """
     section_dir = Path(output_directory) / 'section'
     text_dir = Path(output_directory) / 'text'
     output_path = Path(output_directory) / 'xlsx/consolidated_report.xlsx'
 
-    process_providers(section_dir, text_dir, output_path)
+    all_data = []
+
+    # lire et traiter les donnes des fournisseurs
+    for section_file, text_file in find_file_pairs(section_dir, text_dir):
+        provider, year = get_provider_and_year(text_file.stem)
+        section_names = read_section_names(section_file)
+        data = parse_tsv(text_file, section_names, provider)
+        all_data.append((provider, year, data, section_names))
+
+    # creer le rapport Excel consolide
+    create_consolidated_excel(all_data, output_path)
+
+    # renvoyer le chemin du fichier excel généré
+    return output_path
+
 
 if __name__ == "__main__":
     output_directory = Path('../../outputs')
-    generate_excel_report(output_directory)
+    output_path = generate_excel_report(output_directory)  # capturer le chemin de sortie
+    print(f"Generated Excel report at: {output_path}")  # afficher le chemin du fichier genere
