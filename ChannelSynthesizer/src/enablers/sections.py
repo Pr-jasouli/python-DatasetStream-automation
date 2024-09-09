00188fb1a1e52fc19f3d836ef942513378e83df4
David LJ Botton <david.botton@mail.com>
2024-09-09 17:07:14 +0200

    PDF: updated enabler for section titles generation

--- ChannelSynthesizer/src/enablers/sections.py
+++ ChannelSynthesizer/src/enablers/sections.py
@@ -1,20 +1,11 @@
 import os
 
-from ChannelSynthesizer.src.parsers.all_sections_parser import extract_text, parse, get_provider_colors, detect_provider_and_year, get_pages_to_process, remove_redundant_sections, save_sections
+from parsers.all_sections_parser import extract_text, parse, get_provider_colors, detect_provider_and_year, get_pages_to_process, remove_redundant_sections, save_sections
 
-"""
-This script detects all "menus" or "sections" present in the channel lists (PDFs) of any provider.
-Once determined, this list is sent to script 'B' which cleans the entire PDF as follows:
-                -> row: section name
-                -> row: channel number
-                -> row: channel text (+additional tags for VOO)
-When it's OK, script 'C' formats the result of 'B' into a centralized Excel sheet.
-This separation allows locating when problems occur, with each intermediate result ('A' and 'B') being auditable.
-The utils script contains the precise implementation of each step.
-"""
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 
 def process(folder_path: str) -> None:
-    """Processes all PDF files in the given folder."""
     for file in os.listdir(folder_path):
         if file.endswith(".pdf"):
             path = os.path.join(folder_path, file)
@@ -31,12 +22,14 @@ def process(folder_path: str) -> None:
                     all_sections.extend(sections)
 
                 all_sections = remove_redundant_sections(all_sections)
-                save_sections(path, all_sections)
+
+                output_path = os.path.join(BASE_DIR, 'outputs/section')
+                save_sections(path, all_sections, output_dir=output_path)
                 print(f"Saved sections for {file} for provider {provider} and year {year}")
 
             except ValueError as e:
                 print(f"Error processing {file}: {e}")
 
 if __name__ == "__main__":
-    folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../inputs/pdf'))
+    folder = os.path.join(BASE_DIR, 'inputs/pdf')
     process(folder)
